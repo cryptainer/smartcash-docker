155f661ea543e795a07308d3c0ffbd67564a1848
root <root@vps623881.ovh.net>
2018-12-09 11:31:57 +0100

    added monitoring scripts to be added as cronjobs

---
+++ bin/keepalive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Make sure smartcashd is always running.
+# Add the following to the crontab (i.e. crontab -e)
+# */5 * * * * ~/smartnode/makerun.sh
+
+if ps -A | grep smartcashd > /dev/null
+then
+  exit
+else
+  smartcashd -conf=$WALLET_CONF -datadir=$WALLET_DATA &
+fi
